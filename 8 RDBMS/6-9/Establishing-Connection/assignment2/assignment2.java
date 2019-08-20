4a6d754a70102a758f4c27c7a1438554b7d94f55
MadhavBangaru <bangarumadhav@gmail.com>
2019-08-20 23:31:58 +0530

    first commit

---
+++ 8 RDBMS/6-9/Establishing-Connection/assignment2/assignment2.java
@@ -0,0 +1,22 @@
+package assignment2;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class assignment2 {
+
+	public static void main(String[] args) throws SQLException {
+		Connection conn = null;
+		
+		try {			
+			//Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1531:XE", "scott", "tiger");
+			System.out.println("Connection Established successfully");
+		} catch (Exception e) {
+			System.out.println("Connection could  not be established");
+		} finally {
+			if (conn != null) conn.close();
+		}
+	}
+
+}
---
+++ 8 RDBMS/6-9/Using-PreparedStatement & MetaData-objects/assignment1/DBUtil.java
@@ -0,0 +1,23 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1531:XE", "scott", "tiger");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
